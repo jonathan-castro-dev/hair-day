b40f5feef95103a576070b4fff5c64f9a600bb91
Jonathan Alves <jonathan.calves74@gmail.com>
2026-04-02 23:04:55 -0300

    fix: current date in date-input in the scheduling list

--- src/utils/get-current-date.ts
+++ src/utils/get-current-date.ts
@@ -1,6 +1,6 @@
 export function getCurrentDate() {
   const currentDate = new Date()
-  const currentDay = currentDate.getDate()
+  const currentDay = currentDate.getDate().toString().padStart(2, "0")
   const currentMonth = String(currentDate.getMonth() + 1).padStart(2, "0")
   const currentYear = currentDate.getFullYear()
   const dateArray = [currentYear, currentMonth, currentDay]
